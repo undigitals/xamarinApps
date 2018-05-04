c0a7d68f48c2bcf8cec1bc22240c200c63e46f2e
Umid Negmatullayev <undigitals@gmail.com>
2018-05-05 02:55:45 +0900

    button app

---
+++ test/test/testPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace test
+{
+    public partial class testPage : ContentPage
+    {
+        public testPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title", "Hello world", "ok");
+        }
+
+    }
+}
