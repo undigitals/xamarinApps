df6d5a0c8f883718a6b3a27e289e96d732bbe42f
Umid Negmatullayev <undigitals@gmail.com>
2018-05-05 09:31:37 +0900

    11. giving padding with XAML

--- test/test/testPage.xaml.cs
+++ test/test/testPage.xaml.cs
@@ -10,13 +10,6 @@ namespace test
 
             slider.Value = 0.5;
 
-            if (RuntimePlatform.iOS)
-                Padding = new Thickness(0, 20, 0, 0);
-            else if (Device.OS == TargetPlatform.Android)
-                Padding = new Thickness(10, 20, 0, 0);
-            else if (Device.OS == TargetPlatform.WinPhone)
-                Padding = new Thickness(30, 20, 0, 0);
-            
         }
     }
 }
